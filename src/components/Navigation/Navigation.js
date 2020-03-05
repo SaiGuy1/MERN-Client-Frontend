7adac230fda3e753a21f08aa353d3371d9e2783f
amazingshellyyy <amazingshellyyy@gmail.com>
2020-03-05 01:36:45 -0800

    change color again

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -59,7 +59,7 @@ class Navigation extends React.Component {
   render() {
     //   NAVBAR and MODAL design
     return (
-      <nav className="navbar navbar-expand-lg navbar-dark bg-green">
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
         <Link className="navbar-brand" to='/'>Wayfarer</Link>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
