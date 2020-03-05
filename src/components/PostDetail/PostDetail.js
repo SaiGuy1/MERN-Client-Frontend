3874860a1a0d7f17f742227d8b5186c07c7f38ce
amazingshellyyy <amazingshellyyy@gmail.com>
2020-03-05 12:26:46 -0800

    make data on post detail render for the specific post without hardcoded data

--- src/components/PostDetail/PostDetail.js
+++ src/components/PostDetail/PostDetail.js
@@ -3,12 +3,11 @@ import React, {Component} from "react";
 import axios from "axios";
 
 import PostList from '../PostList/PostList';
-
+import './PostDetails.css'
 class PostDetail extends Component {
 
   state = {
-    content: "",
-    id: ""
+    currentPost:{}
   }
 
   componentDidMount = () => {
@@ -16,42 +15,38 @@ class PostDetail extends Component {
     axios.get(`http://localhost:4000/api/v1/posts/${this.props.match.params.id}` ,{headers: {"authorization": `bearer ${localStorage.getItem('jwt')}`}})
       .then(res=> {
         console.log(res.data);
-        console.log(res.data[0].content);
+        console.log(res.data.location.country);
         this.setState({
           content: res.data.content,
-          id: res.data.id
+          id: res.data.id,
+          currentPost: res.data
         })
-        // this.setState({
-        //
-        // })
-
       })
       .catch( err =>
         console.log(err)
       )
     }
 
-
     render() {
       return(
-        <>
-        <h1> Post Details</h1>
-        <h2>{this.props.match.params.id}</h2>
-        <div class="card mb-3 ml-3 mr-2 shadow p-3 mb-5 bg-white rounded">
-          <div class="row no-gutters">
-          <div class="col-lg-4">
-            <img src={"https://i.ibb.co/M87rPqy/joseph-barrientos-Ji-G7-Bu1-Mo-M-unsplash.jpg"} class="card-img" />
-          </div>
-            <div class="col-md-8">
+        <div className="container mt-5">
+        <div className="card mb-3 ml-3 mr-2 shadow p-3 mb-5 bg-white rounded">
+            <img src={"https://i.ibb.co/M87rPqy/joseph-barrientos-Ji-G7-Bu1-Mo-M-unsplash.jpg"} className="card-img detailpost-image" />
+          
+          <div>
+            {this.state.currentPost.title?<div class="">
               <div class="card-body">
-                <h5 class="card-title">San Francisco</h5>
-                <p class="card-text">{this.state.content}</p>
+              <h3 class="card-title">
+                {this.state.currentPost.title}
+              </h3>
+                <h5>{this.state.currentPost.location.country},{this.state.currentPost.location.city}</h5>
+                <p class="card-text">{this.state.currentPost.content}</p>
                 <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
               </div>
-            </div>
+            </div>:<div>Loading details ...</div>}
           </div>
         </div>
-        </>
+        </div>
       )
     }
 }
