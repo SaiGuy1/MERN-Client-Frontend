5f0d7f81b6f91cbc60679f15cad98b6aeb590746
amazingshellyyy <amazingshellyyy@gmail.com>
2020-03-05 22:32:38 -0800

    edit postDeatils and styleing

--- src/components/PostDetail/PostDetail.js
+++ src/components/PostDetail/PostDetail.js
@@ -1,4 +1,4 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 
 import axios from "axios";
 
@@ -8,55 +8,64 @@ import './PostDetail.css'
 class PostDetail extends Component {
 
   state = {
-    content: "",
+    currentPost: {},
     id: ""
   }
 
   componentDidMount = () => {
 
     const handleDelete = (event) => {
-      axios.delete(`http://localhost:4000/api/v1/posts/${this.props.match.params.id}` ,{headers: {"authorization": `bearer ${localStorage.getItem('jwt')}`}})
+      axios.delete(`http://localhost:4000/api/v1/posts/${this.props.match.params.id}`, { headers: { "authorization": `bearer ${localStorage.getItem('jwt')}` } })
     }
 
 
     console.log("POSTDETAIL")
-    axios.get(`http://localhost:4000/api/v1/posts/${this.props.match.params.id}` ,{headers: {"authorization": `bearer ${localStorage.getItem('jwt')}`}})
-      .then(res=> {
-        console.log(res.data.content);
+    axios.get(`http://localhost:4000/api/v1/posts/${this.props.match.params.id}`, { headers: { "authorization": `bearer ${localStorage.getItem('jwt')}` } })
+      .then(res => {
+        console.log('res.data.content',res.data.content);
+        console.log('res.data',res.data);
         // console.log(res.data[0].content);
         this.setState({
-          content: res.data.content,
-          id: res.data.id
+          
+          id: res.data.id,
+          currentPost: res.data
         })
         // this.setState({
         //
         // })
 
       })
-      .catch( err =>
+      .catch(err =>
         console.log(err)
       )
-    }
+  }
 
 
-    render() {
-      return(
-        <>
-        <h1> Post Details</h1>
-        <h2>{this.props.match.params.id}</h2>
-        <div className="container">
-        <div className="card mb-3 ml-3 mr-2 shadow p-3 mb-5 bg-white rounded" >
-            <img src={"https://i.ibb.co/M87rPqy/joseph-barrientos-Ji-G7-Bu1-Mo-M-unsplash.jpg"} className="card-img" />
-              <div className="card-body">
-                <h5 className="card-title">San Francisco</h5>
-                <p className="card-text">{this.state.content}</p>
-                <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+  render() {
+    return (
+      <>
+        <div className="container mt-5">
+          <div className="row">
+            <div className="col-sm-1 col-lg-3"></div>
+            <div className="col-sm-10  col-lg-6">
+              <div className="card p-3 shadow bg-white rounded singlepost" >
+                <img src={"https://i.ibb.co/M87rPqy/joseph-barrientos-Ji-G7-Bu1-Mo-M-unsplash.jpg"} className="card-img detailpost-image" />
+                <div className="card-body">
+    { this.state.currentPost.location ? <><h3 className="card-title">{this.state.currentPost.title}</h3><h5 className="text-muted">{this.state.currentPost.location.city}, <span>{this.state.currentPost.location.country}</span></h5>
+                  <p className="card-text">{this.state.currentPost.content}</p>
+                  <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p></> : <><h5 className="card-title">San Francisco</h5>
+                  <p className="card-text">content Loading...</p>
+    <p className="card-text"><small className="text-muted">Loading ...</small></p></>}
+                  
+                </div>
               </div>
+            </div>
+            <div className="col-sm-1  col-lg-3"></div>
           </div>
         </div>
-        </>
-      )
-    }
+      </>
+    )
+  }
 }
 
 
